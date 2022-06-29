8a46f6feada886fe8676b79d7c9edf2e84f50865
Macover <macover1507@hotmail.com>
2022-06-29 18:20:19 -0500

    Buscador añadido

--- webScrapy/Form1.Designer.cs
+++ webScrapy/Form1.Designer.cs
@@ -30,26 +30,108 @@ namespace webScrapy
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.txtB_input = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(365, 109);
+            this.button1.Location = new System.Drawing.Point(12, 25);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(221, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Consulta BBC";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtB_input
+            // 
+            this.txtB_input.Enabled = false;
+            this.txtB_input.Location = new System.Drawing.Point(12, 67);
+            this.txtB_input.Name = "txtB_input";
+            this.txtB_input.Size = new System.Drawing.Size(221, 20);
+            this.txtB_input.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.linkLabel1);
+            this.panel1.Controls.Add(this.btnBuscar);
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.txtB_input);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(781, 365);
+            this.panel1.TabIndex = 2;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 330);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(122, 13);
+            this.linkLabel1.TabIndex = 7;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Ir a la pagina de noticias";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Enabled = false;
+            this.btnBuscar.Location = new System.Drawing.Point(12, 102);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(221, 23);
+            this.btnBuscar.TabIndex = 6;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(239, 25);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(530, 329);
+            this.listBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Realizar una busqueda: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(236, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Titulos de noticias encontrados: ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(781, 365);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "WebScrapy";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -57,6 +139,13 @@ namespace webScrapy
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtB_input;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
 
--- webScrapy/Form1.cs
+++ webScrapy/Form1.cs
@@ -14,6 +14,7 @@ namespace webScrapy
 {
     public partial class Form1 : Form
     {
+        private List<string> titulos;
         public Form1()
         {
             InitializeComponent();
@@ -21,10 +22,13 @@ namespace webScrapy
 
         private void button1_Click(object sender, EventArgs e)
         {
+            this.button1.Enabled = false;
+            this.txtB_input.Enabled = true;
+            this.btnBuscar.Enabled = true;
 
             int numeroPaginas = 10;
             
-            List<string> titulos = new List<string>();
+            titulos = new List<string>();            
 
             for (int i = 0; i < numeroPaginas; i++)
             {
@@ -32,16 +36,31 @@ namespace webScrapy
                 url += "="+(i+1);
                 HtmlWeb dWEb = new HtmlWeb();
                 HtmlAgilityPack.HtmlDocument doc = dWEb.Load(url);
-
-                //HtmlNode Body = doc.DocumentNode.CssSelect("body").First();
-                //string bodyHtml = Body.InnerHtml;
-
+                int contador = 1;
                 foreach (var Nodo in doc.DocumentNode.CssSelect(".bbc-uk8dsi"))
                 {
                     titulos.Add(Nodo.InnerHtml);
+                    this.listBox1.Items.Add(contador + ".- " + Nodo.InnerHtml);
+                    contador++;
+                }                
+            }
+        }
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {            
+            string valorABuscar = this.txtB_input.Text;
+            this.listBox1.Items.Clear();
+            foreach (var titulo in titulos)
+            {
+                if (titulo.Contains(valorABuscar))
+                {                    
+                    this.listBox1.Items.Add(titulo);
                 }
             }
+        }
 
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.bbc.com/mundo/topics/cyx5krnw38vt?page=1");
         }
     }
 }
